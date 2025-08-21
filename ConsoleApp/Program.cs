9615c5f88a384402507f3d2781dcdd24d40f3644
Jose Chaverra <miguelchaverratoro@gmail.com>
2025-08-21 09:40:30 -0500

    Avance clase base

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,19 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Vehiculos vehiculo = new Vehiculos();
+vehiculo.Placa = "JJJ000";
+vehiculo.Tipo = "Eléctrico";
+vehiculo.Marca = "Tesla";
+vehiculo.Cc = 1020.0m;
+vehiculo.Fecha = DateTime.Now;
+
+public class Vehiculos
+{
+    public string? Placa;
+    public string? Tipo;
+    public decimal Cc;
+    public string? Marca;
+    public DateTime Fecha;
+}
+
 public class Modos
 {
     public int Id;
     public string? Nombre;
 }
-public class Carros
+public class Carros : Vehiculos
 {
-    public string? Placa;
-    public string? Marca;
-    public decimal cc;
     public int Llantas;
     public bool Funciona;
-    public DateTime Fecha;
     public Modos? Modos;
 
 }
+
