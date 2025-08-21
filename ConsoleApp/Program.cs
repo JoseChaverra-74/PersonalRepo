bcb4289da4fabfd89acd47c43acbb637c0a098ba
Jose Chaverra <miguelchaverratoro@gmail.com>
2025-08-21 09:22:45 -0500

    Clase principal

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Modos
+{
+    public int Id;
+    public string? Nombre;
+}
+public class Carros
+{
+    public string? Placa;
+    public string? Marca;
+    public decimal cc;
+    public int Llantas;
+    public bool Funciona;
+    public DateTime Fecha;
+    public Modos? Modos;
+
+}
